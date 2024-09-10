3d3195b1efdc8856f6258208028a8c21ea517974
Code-Ninjas-Stouffville <codeninjasstouffville@outlook.com>
2024-09-10 18:18:30 -0400

    Final Touches Part 1
    finishing the final touches.. will take a week or two more

--- Assets/Scripts/Exit.cs
+++ Assets/Scripts/Exit.cs
@@ -6,8 +6,9 @@ using UnityEngine.SceneManagement;
 public class Exit : MonoBehaviour
 {
     SpriteRenderer m_renderer;
-    public GameObject gem;
+    public GameObject collectible;
     public GameObject background;
+    public GemChecker gem;
     public string teleportDestination;
 
     private void Start()
@@ -18,6 +19,41 @@ public class Exit : MonoBehaviour
     void OnTriggerEnter2D(Collider2D col)
     {
         if(col.gameObject.tag == "Player")
+        {
+            
+        }
+
+        if (col.gameObject.tag == "Player" && gem.green == true)
+        {
+
+        }
+
+        if (col.gameObject.tag == "Player" && gem.blue == true)
+        {
+
+        }
+
+        if (col.gameObject.tag == "Player" && gem.red == true)
+        {
+
+        }
+
+        if (col.gameObject.tag == "Player" && gem.green == true && gem.blue == true)
+        {
+
+        }
+
+        if (col.gameObject.tag == "Player" && gem.green == true && gem.red == true)
+        {
+
+        }
+
+        if (col.gameObject.tag == "Player" && gem.blue == true && gem.red == true)
+        {
+
+        }
+
+        if(col.gameObject.tag == "Player" && gem.green == true && gem.blue == true && gem.red == true)
         {
             SceneManager.LoadScene(teleportDestination);
         }
